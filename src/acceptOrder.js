73c4275074300a93306bc1ffcec06a667e29f647
SnyderMbishai <csmbishai@gmail.com>
2018-10-17 13:52:02 +0300

    feat(orders): sort and display accepted orders

---
+++ src/acceptOrder.js
@@ -0,0 +1,45 @@
+import { getData } from "./common";
+import swal from "sweetalert";
+
+let username = document.getElementById("admin");
+let loggedInAdmin = localStorage.getItem("username");
+username.innerHTML = `Admin logged in as:${loggedInAdmin}`;
+
+// getData all orders
+let accessToken = localStorage.getItem("token");
+let url = "/orders";
+getData(url, accessToken)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+    if (response) {
+      let orders = response["orders"];
+      console.log(orders);
+      for (let i in orders) {
+        if (orders.hasOwnProperty(i)) {
+          let order = orders[i];
+          // meal properties
+          let meals = order.meals;
+          for (let i in meals) {
+            if (meals.hasOwnProperty(i)) {
+              let meal = meals[i];
+              if (order.accepted === true) {
+                console.log("Aye");
+                document.getElementById("accepted").innerHTML += `<tr>
+                      <td>${order.username}</td>
+                      <td>${meal.meal_name}</td>
+                      <td>${meal.quantity}</td>
+                      <td>${order.created_at}</td>
+                      <td>
+                          <button onclick="completeOrder(event)" id="acpt" data-id="${
+                            order.order_id
+                          }">complete</button>
+                      </td>
+                  </tr>`;
+              }
+            }
+          }
+        }
+      }
+    }
+  });
