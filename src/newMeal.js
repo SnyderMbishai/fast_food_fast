c7ab7229266a2b0b7cbffc5d526416d166a4176a
SnyderMbishai <csmbishai@gmail.com>
2018-10-29 10:22:17 +0300

    feat(images): add images to meals

--- src/newMeal.js
+++ src/newMeal.js
@@ -5,10 +5,14 @@ document.getElementById("newMealForm").addEventListener("submit", function(e) {
   e.preventDefault();
   const name = document.getElementById("mealName").value;
   const price = document.getElementById("price").value;
+  let img = document.getElementById("img").value;
+  img = img.substring(img.lastIndexOf("\\") + 1, img.length);
+  console.log(img.substring(img.lastIndexOf("\\") + 1, img.length, 362));
 
   const data = {
     name: name,
-    price: price
+    price: price,
+    img: img
   };
   const url = "/meals";
   let accessToken = localStorage.getItem("token");
@@ -19,11 +23,11 @@ document.getElementById("newMealForm").addEventListener("submit", function(e) {
       if (response) {
         swal(response.message).then(() => {
           if (response.message === "Meal successfully added.") {
-            document.getElementById("new").innerHTML += `<div><figure>
-                    <img src="../static/images/3.jpg" alt="food option">
-                    <figcaption>${response.name}</figcaption>
-                </figure>
-                <p>price:${response.price}</p></div>`;
+            // document.getElementById("new").innerHTML += `<div><figure>
+            //         <img src="../static/images/3.jpg" alt="food option">
+            //         <figcaption>${response.name}</figcaption>
+            //     </figure>
+            //     <p>price:${response.price}</p></div>`;
             window.location.href = "/admin_dashboard.html";
           }
           window.location.href = "/admin_dashboard.html";
