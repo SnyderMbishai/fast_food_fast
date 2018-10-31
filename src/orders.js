e690cc821211621a17232fd54c4cad270d828515
SnyderMbishai <csmbishai@gmail.com>
2018-10-31 16:12:33 +0300

    Add function for completing orders

--- src/orders.js
+++ src/orders.js
@@ -8,6 +8,27 @@ username.innerHTML = `Admin logged in as:${loggedInAdmin}`;
 // getData all orders
 let accessToken = localStorage.getItem("token");
 let url = "/orders";
+
+function completeOrder(event) {
+  event.preventDefault();
+  let id = event.currentTarget.dataset.id;
+  let accessToken = localStorage.getItem("token");
+  let url = `https://fastfoodfastapi1n2.herokuapp.com/api/v2/orders/${id}`;
+  fetch(url, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "content-type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(response => {
+      swal(response.message).then(() => {
+        window.location.replace("accepted_orders.html");
+      });
+    });
+}
+
 getData(url, accessToken)
   .then(response => response.json())
   .then(response => {
@@ -23,10 +44,12 @@ getData(url, accessToken)
           for (let i in meals) {
             if (meals.hasOwnProperty(i)) {
               let meal = meals[i];
-              console.log(meal.quantity);
-
+              // console.log(meal.quantity);
               console.log(order.meals);
-              document.getElementById("new").innerHTML += `<tr>
+              console.log(meal.started);
+              if (order.started === false) {
+                // console.log(meal.started, "asdfghjklfghj");
+                document.getElementById("new").innerHTML += `<tr>
                     <td>${order.username}</td>
                     <td>${meal.meal_name}</td>
                     <td>${meal.quantity}</td>
@@ -40,6 +63,36 @@ getData(url, accessToken)
                         }">decline</button>
                     </td>
                 </tr>`;
+              } else {
+                if (order.completed == false) {
+                  if (order.accepted === true) {
+                    document.getElementById("accepted").innerHTML += `<tr>
+                    <td>${order.username}</td>
+                    <td>${meal.meal_name}</td>
+                    <td>${meal.quantity}</td>
+                    <td>${order.created_at}</td>
+                    <td>
+                        <button onClick="completeOrder(event)" id="acpt" data-id="${
+                          order.order_id
+                        }">complete</button>
+
+                    </td>
+                </tr>`;
+                  } else {
+                    document.getElementById("rejected").innerHTML += `<tr>
+                    <td>${order.username}</td>
+                    <td>${meal.meal_name}</td>
+                    <td>${meal.quantity}</td>
+                    <td>${order.created_at}</td>
+                    <td>
+                        <button onClick="completeOrder(event)" id="acpt" data-id="${
+                          order.order_id
+                        }">complete</button>
+
+                </tr>`;
+                  }
+                }
+              }
             }
           }
         }
